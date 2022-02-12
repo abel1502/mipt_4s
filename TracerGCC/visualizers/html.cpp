d636a37f9a238b2cf6d9522de41132a10a7afba8
abel1502 <terragame.abel@gmail.com>
2022-02-12 18:03:26 +0300

    TracerGCC partially implement html visualizer

--- TracerGCC/visualizers/html.cpp
+++ TracerGCC/visualizers/html.cpp
@@ -1,2 +1,46 @@
 #include "html.h"
 
+
+#pragma region HtmlTag
+HtmlTag::WriteProxy HtmlTag::writeOpen(OutputFile &ofile) {
+    ofile.write("<");
+    ofile.write(tag);
+    
+    return WriteProxy{ofile, false};
+}
+
+void HtmlTag::writeClose(OutputFile &ofile) {
+    ofile.write("</");
+    ofile.write(tag);
+    ofile.write(">");
+}
+
+HtmlTag::WriteProxy HtmlTag::writeInline(OutputFile &ofile) {
+    ofile.write("<");
+    ofile.write(tag);
+    
+    return WriteProxy{ofile, true};
+}
+
+
+HtmlTag::WriteProxy::WriteProxy(OutputFile &ofile_, bool isInline_) :
+    ofile{ofile_}, isInline{isInline_} {}
+
+HtmlTag::WriteProxy::~WriteProxy() {
+    writeEnd();
+}
+
+HtmlTag::WriteProxy &HtmlTag::WriteProxy::arg(const char *name, const std::string_view &value) {
+    ofile.write(" ");
+    ofile.write(name);
+    ofile.write("=\"");
+    ofile.write(value);
+    ofile.write("\"");
+
+    return *this;
+}
+
+void HtmlTag::WriteProxy::writeEnd() const {
+    ofile.write(isInline ? "/>" : ">");
+}
+#pragma endregion HtmlTag
--- TracerGCC/visualizers/html.h
+++ TracerGCC/visualizers/html.h
@@ -1,3 +1,60 @@
 #pragma once
 #include "../general.h"
+#include "visualizer.h"
+#include "../stdfs.h"
+#include <fstream>
+#include <vector>
 
+
+class HtmlTag {
+protected:
+    class WriteProxy;
+
+public:
+    // You should avoid saving the result into 
+    WriteProxy writeOpen(OutputFile &ofile);
+    void writeClose(OutputFile &ofile);
+    WriteProxy writeInline(OutputFile &ofile);
+
+protected:
+    const char *tag = "";
+
+
+    class WriteProxy {
+    public:
+        WriteProxy &arg(const char *name, const std::string_view &value);
+
+        ~WriteProxy();
+
+    protected:
+        OutputFile &ofile;
+        bool isInline = false;
+
+
+        WriteProxy(OutputFile &ofile_, bool isInline_ = false);
+
+        void writeEnd() const;
+
+
+        friend HtmlTag;
+    };
+
+};
+
+
+class HtmlTraceVisualizer : public TraceVisualizer {
+public:
+    inline HtmlTraceVisualizer(const std::fs::path &outputFile, bool cumulative = false) :
+        TraceVisualizer(outputFile, cumulative) {}
+
+    virtual void visualize(const Trace &trace) override;
+
+    virtual ~HtmlTraceVisualizer() override;
+
+protected:
+    std::vector<HtmlTag> tagStack{};
+
+    void beginLog();
+    void endLog();
+
+};
