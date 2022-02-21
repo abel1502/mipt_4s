266135a0afa89560f9097a84d3854222b9abc3f7
abel1502 <terragame.abel@gmail.com>
2022-02-21 20:23:39 +0300

    TracerGCC tons of dot progress
    (Still not done though)

--- TracerGCC/visualizers/dot.cpp
+++ TracerGCC/visualizers/dot.cpp
@@ -1,45 +1,52 @@
 #include "dot.h"
 #include <sstream>
 #include <cstdarg>
+#include <algorithm>
 
 
 #pragma region DotOutput
 DotOutput::ArgsWriteProxy DotOutput::writeNode(const std::string_view &name) {
-    ofile.write("\"");
+    // ofile.write("\"");
     ofile.write(name);
-    ofile.write("\" ");
+    // ofile.write("\" ");
+    ofile.write(" ");
 
     return ArgsWriteProxy(ofile);
 }
 
 DotOutput::ArgsWriteProxy DotOutput::writefNode(const char *fmt, ...) {
-    ofile.write("\"");
+    // ofile.write("\"");
     va_list args{};
     va_start(args, fmt);
     ofile.writefv(fmt, args);
     va_end(args);
-    ofile.write("\" ");
+    // ofile.write("\" ");
+    ofile.write(" ");
 
     return ArgsWriteProxy(ofile);
 }
 
-DotOutput::ArgsWriteProxy DotOutput::writeEdge(const std::string_view &from, const std::string_view &to) {
-    ofile.write("\"");
+DotOutput::ArgsWriteProxy DotOutput::writeEdge(const std::string_view &from,
+                                               const std::string_view &to, bool constraint) {
+    // ofile.write("\"");
     ofile.write(from);
-    ofile.write("\" -> \"");
+    // ofile.write("\" -> \"");
+    ofile.write(" -> ");
     ofile.write(to);
-    ofile.write("\" ");
+    // ofile.write("\" ");
+    ofile.write(" ");
 
-    return ArgsWriteProxy(ofile);
+    return std::move(ArgsWriteProxy(ofile).arg("constraint", constraint ? "true" : "false"));
 }
 
 [[nodiscard]] DotOutput::EdgeFmtWriteProxy DotOutput::writefEdge(const char *fmtFrom, ...) {
-    ofile.write("\"");
+    // ofile.write("\"");
     va_list args{};
     va_start(args, fmtFrom);
     ofile.writefv(fmtFrom, args);
     va_end(args);
-    ofile.write("\" -> ");
+    // ofile.write("\" -> ");
+    ofile.write(" -> ");
 
     return EdgeFmtWriteProxy(ofile);
 }
@@ -58,9 +65,15 @@ void DotOutput::endGraph() {
     ofile.write("}\n");
 }
 
+DotOutput::ArgsWriteProxy DotOutput::writeDefaultArgs(const char *type) {
+    ofile.write(type);
+    ofile.write(" ");
+    return ArgsWriteProxy{ofile};
+}
+
 void DotOutput::writeArg(const char *name, const std::string_view &value) {
     ArgsWriteProxy{ofile, false}.arg(name, value);
-    ofile.write(";\n");
+    ofile.write("\n");
 }
 
 void DotOutput::writeArg(const char *name, const char *fmt, ...) {
@@ -68,13 +81,34 @@ void DotOutput::writeArg(const char *name, const char *fmt, ...) {
     va_start(args, fmt);
     ArgsWriteProxy{ofile, false}.argfv(name, fmt, args);
     va_end(args);
-    ofile.write(";\n");
+    ofile.write("\n");
+}
+
+std::string DotOutput::readTpl(const std::string_view &name) {
+    std::fs::path tplPath = std::fs::current_path()
+        .append("../tpl/").append(name).replace_extension(".tpl");
+    
+    std::ifstream file{tplPath, file.binary | file.in};  // Binary to avoid size changes
+
+    REQUIRE(!file.bad());
+
+    file.seekg(0, file.end);
+    size_t size = file.tellg();
+    file.seekg(0, file.beg);
+
+    std::string buf{};
+    buf.resize(size);
+    file.read(buf.data(), size);
+
+    return buf;
 }
 
 
 #pragma region ArgsWriteProxy
 DotOutput::ArgsWriteProxy::~ArgsWriteProxy() {
-    writeEnd();
+    if (!disabled) {
+        writeEnd();
+    }
 }
 
 DotOutput::ArgsWriteProxy::ArgsWriteProxy(OutputFile &ofile_, bool braces_) :
@@ -86,12 +120,23 @@ DotOutput::ArgsWriteProxy::ArgsWriteProxy(OutputFile &ofile_, bool braces_) :
 }
 
 void DotOutput::ArgsWriteProxy::writeEnd() const {
+    assert(!disabled);
+
     if (braces) {
         ofile.write("]\n");
     }
 }
 
+DotOutput::ArgsWriteProxy::ArgsWriteProxy(ArgsWriteProxy &&other) :
+    ofile{other.ofile},
+    braces{other.braces} {
+    
+    other.disabled = true;
+}
+
 DotOutput::ArgsWriteProxy &DotOutput::ArgsWriteProxy::arg(const char *name, const std::string_view &value) {
+    assert(!disabled);
+
     ofile.write(name);
     ofile.write("=\"");
     ofile.write(value);
@@ -101,6 +146,8 @@ DotOutput::ArgsWriteProxy &DotOutput::ArgsWriteProxy::arg(const char *name, cons
 }
 
 DotOutput::ArgsWriteProxy &DotOutput::ArgsWriteProxy::argf(const char *name, const char *fmt, ...) {
+    assert(!disabled);
+
     va_list args{};
     va_start(args, fmt);
     argfv(name, fmt, args);
@@ -110,29 +157,42 @@ DotOutput::ArgsWriteProxy &DotOutput::ArgsWriteProxy::argf(const char *name, con
 }
 
 DotOutput::ArgsWriteProxy &DotOutput::ArgsWriteProxy::argfv(const char *name, const char *fmt, va_list args) {
+    assert(!disabled);
+
     ofile.write(name);
     ofile.write("=\"");
     ofile.writefv(fmt, args);
-    ofile.write("\" ");
+    ofile.write("\"; ");
 
     return *this;
 }
 #pragma endregion ArgsWriteProxy
 
 #pragma region EdgeFmtWriteProxy
+DotOutput::EdgeFmtWriteProxy::EdgeFmtWriteProxy(EdgeFmtWriteProxy &&other) :
+    ofile{other.ofile}, willConstraint{other.willConstraint} {
+
+    other.disabled = true;
+}
+
 DotOutput::EdgeFmtWriteProxy::~EdgeFmtWriteProxy() {
-    writeEnd();
+    if (!disabled) {
+        writeEnd();
+    }
 }
 
 DotOutput::ArgsWriteProxy DotOutput::EdgeFmtWriteProxy::to(const char *fmtTo, ...) {
-    ofile.write("\"");
+    assert(!disabled);
+
+    // ofile.write("\"");
     va_list args{};
     va_start(args, fmtTo);
     ofile.writefv(fmtTo, args);
     va_end(args);
-    ofile.write("\" ");
+    // ofile.write("\" ");
+    ofile.write(" ");
 
-    return ArgsWriteProxy(ofile);
+    return std::move(ArgsWriteProxy(ofile).arg("constraint", willConstraint ? "true" : "false"));
 }
 
 DotOutput::EdgeFmtWriteProxy::EdgeFmtWriteProxy(OutputFile &ofile_) :
@@ -141,16 +201,24 @@ DotOutput::EdgeFmtWriteProxy::EdgeFmtWriteProxy(OutputFile &ofile_) :
 void DotOutput::EdgeFmtWriteProxy::writeEnd() const {}
 #pragma endregion EdgeFmtWriteProxy
 
+#pragma region HtmlLabelWriteProxy
+// TODO
+#pragma endregion HtmlLabelWriteProxy
+
 #pragma endregion DotOutput
 
 
 #pragma region DotTraceVisualizer
 void DotTraceVisualizer::visualize(const Trace &trace) {
+    stats = {};
+    
     beginLog();
 
     const auto &entries = trace.getEntries();
+    nodesCnt = entries.size();
+    nodesPresent.assign(nodesCnt, true);
 
-    for (unsigned i = 0; i < entries.size(); ++i) {
+    for (unsigned i = 0; i < nodesCnt; ++i) {
         logEntry(trace, i);
     }
 
@@ -170,78 +238,158 @@ void DotTraceVisualizer::beginLog() {
 
     dot.beginGraph("digraph", "Log");
 
-    // TODO: A header
-    dot.writeNode("info")
-        .arg("fontname", "Times")
-        .arg("shape", "box")
-        .labelt(std::fs::current_path().append("../tpl/label_info.tpl"), true);
+    dot.writeDefaultArgs("graph").arg("fontname", "Times");
+    dot.writeDefaultArgs("node") .arg("fontname", "Times");
+    dot.writeDefaultArgs("edge") .arg("fontname", "Times");
+
+    dot.writeArg("rankdir", "TB");
+    dot.writeArg("newrank", "true");
+
+    dumpLegend();
+    // Intentionally missing, due to it needing the summary data that's only available in the end
+    // dumpInfo();
+
+    dumpPadding();
+
+    dot.beginGraph("subgraph", "cluster_log");
 }
 
 void DotTraceVisualizer::endLog() {
+    dot.endGraph();
+
+    dumpInfo();
+
+    dumpOrdering();
+
     dot.endGraph();
 
     ofile.forceFlush();
     ofile.close();
 }
 
+void DotTraceVisualizer::dumpInfo() {
+    dot.beginGraph("subgraph", "cluster_metadata");
+    dot.writeNode("info")
+        .arg("fontname", "Consolas")
+        .arg("shape", "plain")
+        .labelt("label_info",
+                stats.copies,
+                stats.moves,
+                stats.copies + stats.moves);
+    dot.endGraph();
+}
+
+void DotTraceVisualizer::dumpLegend() {
+    dot.beginGraph("subgraph", "cluster_metadata");
+    dot.beginGraph("subgraph", "cluster_legend");
+    dot.writeArg("label", "Legend");
+
+    dot.beginGraph("subgraph", "");
+    dot.writeArg("rank", "same");
+
+    dot.writeNode("legend_names")
+        .labelt("label_legend",
+                "timeline&nbsp;",
+                "variable lifespan&nbsp;",
+                "variable usage&nbsp;")
+        .arg("shape", "plaintext");
+    
+    dot.writeNode("legend_targets")
+        .labelt("label_legend",
+                " ",
+                " ",
+                " ")
+        .arg("shape", "plaintext");
+
+    dot.endGraph();
+
+    dot.writeEdge("legend_names:1:e", "legend_targets:1:w", true)
+        .arg("color", style::edge_timeline_color)
+        .arg("style", style::edge_timeline_style);
+    
+    dot.writeEdge("legend_names:2:e", "legend_targets:2:w", true)
+        .arg("color", style::edge_lifespan_color)
+        .arg("style", style::edge_lifespan_style);
+    
+    dot.writeEdge("legend_names:3:e", "legend_targets:3:w", true)
+        .arg("color", style::edge_usage_color)
+        .arg("style", style::edge_usage_style);
+
+    dot.endGraph();
+    dot.endGraph();
+}
+
+void DotTraceVisualizer::dumpOrdering() {
+    unsigned lastNode = 0;
+
+    while (lastNode < nodesCnt && !nodesPresent[lastNode]) {
+        ++lastNode;
+    }
+
+    for (unsigned node = lastNode + 1; node < nodesCnt; ++node) {
+        if (!nodesPresent[node]) {
+            continue;
+        }
+
+        dot.writefEdge(NODE_FMT, lastNode).constraint().to(NODE_FMT, node)
+            .arg("style", style::edge_timeline_style)
+            .arg("color", style::edge_timeline_color);
+
+        lastNode = node;
+    }
+}
+
+void DotTraceVisualizer::dumpPadding() {
+    dot.beginGraph("", "");
+    dot.writeDefaultArgs("node")
+        .arg("style", "invis");
+    dot.writeNode("pad_1");
+    dot.writeNode("pad_2");
+    dot.endGraph();
+    dot.writeEdge("pad_1", "pad_2")
+        .arg("style", "invis");
+    dot.writeEdge("pad_2", "node_0")
+        .arg("style", "invis");
+}
+
 void DotTraceVisualizer::logEntry(const Trace &trace, unsigned idx) {
     const TraceEntry &entry = trace.getEntries()[idx];
     
     switch (entry.op) {
-    #if 0
     case TracedOp::FuncEnter: {
         // A new function was entered
 
-        logIndent();
-        openTag("span").arg("class", "func-info");
-        ofile.write(entry.place.func.function_name());
-        ofile.write(" {\n");
-        closeTag();
+        dot.beginGraph("subgraph", std::string("cluster_func_") + std::to_string(idx));
 
-        recursionDepth = entry.place.recursionDepth;
+        dot.writeArg("style", "filled");
+        dot.writeArg("color", getFuncColor(entry.place.recursionDepth));
+        dot.writeArg("label", entry.place.func.function_name());
+        dot.writeArg("labeljust", "l");
+
+        nodesPresent[idx] = false;
     } break;
     
     case TracedOp::FuncLeave: {
         // A function was exited
 
-        recursionDepth = entry.place.recursionDepth;
+        dot.endGraph();
 
-        logIndent();
-        openTag("span").arg("class", "func-info");
-        ofile.write("}\n");
-        closeTag();
+        nodesPresent[idx] = false;
     } break;
 
     case TracedOp::DbgMsg: {
-        logIndent();
-
-        openTag("span").arg("class", "dbg-msg");
-        ofile.write("[dbg] ");
-        openTag("i");
-        ofile.write(entry.opStr);
-        closeTag();
-        closeTag();
-
-        ofile.writeln();
+        writeNode(idx, true)
+            .label("[dbg] <i>%s</i>", entry.opStr);
     } break;
 
     case TracedOp::Ctor: {
-        logIndent();
-
-        openTag("span")
-            .arg("class", "label label-ctor")
-            .argf("id", "label-ctor-%u", entry.inst.idx);
-        openTag("a").argf("href", "#label-dtor-%u", entry.inst.idx);
-        ofile.write("ctor");
-        closeTag();
-        closeTag();
-        logVarInfo(entry.inst);
-
-        ofile.writeln();
-
-        onVarCreated(entry.inst.idx);
+        writeNode(idx)
+            .labelt("label_unary", "green", "ctor", buildVarInfo(entry.inst).c_str());
+        
+        vars.onCtor(idx, entry.inst);
     } break;
 
+    #if 0
     case TracedOp::Dtor: {
         logIndent();
 
@@ -332,8 +480,10 @@ void DotTraceVisualizer::logEntry(const Trace &trace, unsigned idx) {
     #endif
 
     // TODO: Remove
-    default:
-        break;
+    default: {
+        writeNode(idx, true)
+            .label("Placeholder for op #%u", idx);
+    } break;
 
     // NODEFAULT
     }
@@ -362,6 +512,121 @@ void DotTraceVisualizer::compileOutput(bool debug) const {
     }
 }
 
+std::string_view DotTraceVisualizer::getFuncColor(unsigned level) {
+    constexpr std::string_view COLORS[] = {
+        "#ffffff",
+        "#efefef",
+        "#dfdfdf",
+        "#cfcfcf",
+        "#bfbfbf",
+        "#afafaf",
+        "#9f9f9f",
+        "#8f8f8f",
+    };
+
+    constexpr unsigned NUM_COLORS = sizeof(COLORS) / sizeof(COLORS[0]);
+
+    if (level >= NUM_COLORS) {
+        level = NUM_COLORS - 1;
+    }
+
+    return COLORS[level];
+}
+
+auto DotTraceVisualizer::writeNode(unsigned entryIdx, bool box) -> decltype(dot.writeNode("")) {
+    return std::move(
+        dot.writefNode(NODE_FMT, entryIdx)
+            .arg("shape", box ? "box" : "plain")
+    );
+}
+
+std::string DotTraceVisualizer::buildVarInfo(const TraceEntry::VarInfo &info) {
+    #if 0
+        <tr>
+            <td align="left" border="1">%s</td> <!-- name -->
+            <td align="left" border="1">#%u</td> <!-- idx -->
+            <td align="left" border="1">%p:%u </td> <!-- ptr, ptrCell -->
+            <td align="left" border="1">=%s</td> <!-- value -->
+        </tr>
+        <tr>
+            <td align="left" colspan="4" border="1">%s</td> <!-- history -->
+        </tr>;
+    #endif
+
+    std::string fmt = dot.readTpl("varinfo");
+
+    // Args: name, idx, ptr, ptrCell, value, history
+    std::string result = helpers::sprintfxx(fmt.c_str(),
+        info.name, info.idx, info.addr, ptrCells.get(info.addr),
+        info.valRepr.c_str(), vars[info].expression.c_str()
+    );
+
+    return result;
+}
+
+#pragma region Vars
+DotTraceVisualizer::VarState &DotTraceVisualizer::Vars::getVarState(unsigned varIdx) {
+    if (varIdx >= vars.size()) {
+        vars.resize(std::max((size_t)varIdx + 1, vars.capacity() * 2));
+    }
+
+    return vars.at(varIdx);
+}
+
+void DotTraceVisualizer::Vars::onCtor(unsigned opIdx, const TraceEntry::VarInfo &info) {
+    auto &state = getVarState(info);
+
+    state.ctorOpIdx = opIdx;
+    state.lastChangeOpIdx = opIdx;
+    state.lastUseOpIdx = opIdx;
+    state.expression = info.name;
+    state.lastAddr = info.addr;
+}
+
+void DotTraceVisualizer::Vars::onUsed(unsigned opIdx, const TraceEntry::VarInfo &info) {
+    auto &state = getVarState(info);
+
+    state.lastUseOpIdx = opIdx;
+    state.lastAddr = info.addr;
+}
+
+void DotTraceVisualizer::Vars::onDtor(unsigned opIdx, const TraceEntry::VarInfo &info) {
+    auto &state = getVarState(info);
+
+    state.dtorOpIdx = opIdx;
+    state.lastChangeOpIdx = opIdx;
+    state.lastUseOpIdx = opIdx;
+    state.lastAddr = info.addr;
+}
+
+void DotTraceVisualizer::Vars::onChanged(unsigned opIdx, const TraceEntry::VarInfo &info,
+                                         std::function<void (std::string &curExpr)> operation) {
+    auto &state = getVarState(info);
+
+    state.lastChangeOpIdx = opIdx;
+    state.lastUseOpIdx = opIdx;
+    state.lastAddr = info.addr;
+    operation(state.expression);
+}
+
+void DotTraceVisualizer::Vars::onChanged(unsigned opIdx, const TraceEntry::VarInfo &info,
+                                         const TraceEntry::VarInfo &otherInfo,
+                                         std::function<void (std::string &curExpr,
+                                         const std::string_view &otherExpr)> operation) {
+    auto &state      = getVarState(info);
+    auto &stateOther = getVarState(otherInfo);
+
+    state.lastChangeOpIdx = opIdx;
+    state.lastUseOpIdx = opIdx;
+    state.lastAddr = info.addr;
+
+    stateOther.lastUseOpIdx = opIdx;
+    stateOther.lastAddr = info.addr;
+
+    operation(state.expression, stateOther.expression);
+}
+
+#pragma endregion Vars
 
 #pragma endregion DotTraceVisualizer
 
--- TracerGCC/visualizers/dot.h
+++ TracerGCC/visualizers/dot.h
@@ -2,14 +2,18 @@
 #include "../general.h"
 #include "visualizer.h"
 #include "../helpers.h"
+#include "html.h"  // For html tag output
 #include <vector>
 #include <map>
+#include <functional>
 
 
+#pragma region DotOutput
 class DotOutput {
 protected:
     class ArgsWriteProxy;
     class EdgeFmtWriteProxy;
+    class HtmlLabelWriteProxy;
 
 public:
     inline DotOutput(OutputFile &ofile_) :
@@ -18,7 +22,7 @@ public:
     ArgsWriteProxy writeNode(const std::string_view &name);
     ArgsWriteProxy writefNode(const char *fmt, ...);
 
-    ArgsWriteProxy writeEdge(const std::string_view &from, const std::string_view &to);
+    ArgsWriteProxy writeEdge(const std::string_view &from, const std::string_view &to, bool constraint = false);
     [[nodiscard]] EdgeFmtWriteProxy writefEdge(const char *fmtFrom, ...);
 
     void beginGraph(const char *type, const std::string_view &name, bool strict = false);
@@ -30,45 +34,51 @@ public:
 
     void writeArg(const char *name, const char *fmt, ...);
 
+    static std::string readTpl(const std::string_view &name);
+
 protected:
     OutputFile &ofile;
 
 
     class ArgsWriteProxy {
     public:
+        ArgsWriteProxy(const ArgsWriteProxy &other) = delete;
+        ArgsWriteProxy &operator=(const ArgsWriteProxy &other) = delete;
+
+        ArgsWriteProxy(ArgsWriteProxy &&other);
+        ArgsWriteProxy &operator=(ArgsWriteProxy &&other) = delete;
+
         ArgsWriteProxy &arg(const char *name, const std::string_view &value);
 
         ArgsWriteProxy &argf(const char *name, const char *fmt, ...);
 
         ArgsWriteProxy &argfv(const char *name, const char *fmt, va_list args);
 
+        /// The label() and labelt() functions treat their data as html.
+        /// If you want a plaintext label, use arg("label", ...) instead
         template <typename ... As>
-        ArgsWriteProxy &labelt(const std::fs::path &tplPath, bool html, As &&... args)  {
-            std::ifstream file{tplPath, file.binary | file.in};  // Binary to avoid size changes
-
-            REQUIRE(!file.bad());
-
-            file.seekg(0, file.end);
-            size_t size = file.tellg();
-            file.seekg(0, file.beg);
-
-            std::string buf{};
-            buf.resize(size);
-            file.read(buf.data(), size);
-
+        ArgsWriteProxy &label(const char *fmt, As &&... args) {
             ofile.write("label");
-            ofile.write(html ? "=<" : "=\"");
-            ofile.writef(buf.c_str(), std::forward<As>(args)...);
-            ofile.write(html ? ">; " : "\"; ");
+            ofile.write("=<");
+            ofile.writef(fmt, std::forward<As>(args)...);
+            ofile.write(">; ");
 
             return *this;
         }
+
+        template <typename ... As>
+        ArgsWriteProxy &labelt(const std::string_view &tpl, As &&... args)  {
+            return label(readTpl(tpl).c_str(), std::forward<As>(args)...);
+        }
+
+        HtmlLabelWriteProxy labelh();
 
         ~ArgsWriteProxy();
 
     protected:
         OutputFile &ofile;
         bool braces = true;
+        bool disabled = false;
 
 
         ArgsWriteProxy(OutputFile &ofile_, bool braces_ = true);
@@ -82,12 +92,26 @@ protected:
 
     class EdgeFmtWriteProxy {
     public:
+        EdgeFmtWriteProxy(const EdgeFmtWriteProxy &other) = delete;
+        EdgeFmtWriteProxy &operator=(const EdgeFmtWriteProxy &other) = delete;
+
+        EdgeFmtWriteProxy(EdgeFmtWriteProxy &&other);
+        EdgeFmtWriteProxy &operator=(EdgeFmtWriteProxy &&other) = delete;
+
         ~EdgeFmtWriteProxy();
+
+        inline EdgeFmtWriteProxy &constraint(bool value = true) {
+            willConstraint = value;
+
+            return *this;
+        }
 
         ArgsWriteProxy to(const char *fmtTo, ...);
 
     protected:
         OutputFile &ofile;
+        bool willConstraint = false;
+        bool disabled = false;
 
 
         EdgeFmtWriteProxy(OutputFile &ofile_);
@@ -98,10 +122,86 @@ protected:
         friend DotOutput;
     };
 
+    class HtmlLabelWriteProxy {
+    public:
+        HtmlLabelWriteProxy(const HtmlLabelWriteProxy &other) = delete;
+        HtmlLabelWriteProxy &operator=(const HtmlLabelWriteProxy &other) = delete;
+
+        HtmlLabelWriteProxy(HtmlLabelWriteProxy &&other);
+        HtmlLabelWriteProxy &operator=(HtmlLabelWriteProxy &&other) = delete;
+
+        ArgsWriteProxy finish();
+
+        const HtmlTag &pushTag(const char *tag);
+        HtmlTag popTag();
+
+        inline auto openTag(const char *tag);
+        inline auto inlineTag(const char *tag);
+        inline void closeTag(const char *tag);
+        inline void closeTag();
+
+    protected:
+        ArgsWriteProxy parent;
+        OutputFile &ofile;
+        std::vector<HtmlTag> tagStack{};
+        bool disabled = false;
+
+
+        HtmlLabelWriteProxy(ArgsWriteProxy &&parent_, OutputFile &ofile_);
+
+        void writeEnd();
+
+
+        friend DotOutput;
+        friend ArgsWriteProxy;
+    };
+
 };
+#pragma endregion DotOutput
 
 
 class DotTraceVisualizer : public TraceVisualizer {
+#pragma region structs
+protected:
+    struct VarState {
+        static constexpr unsigned BAD_IDX = -1u;
+
+        unsigned ctorOpIdx = BAD_IDX;
+        unsigned lastUseOpIdx = BAD_IDX;
+        unsigned lastChangeOpIdx = BAD_IDX;
+        unsigned dtorOpIdx = BAD_IDX;
+
+        const void *lastAddr = nullptr;
+
+        std::string expression{""};
+
+
+        VarState() = default;
+    };
+
+    class Vars {
+    public:
+        VarState &getVarState(unsigned varIdx);
+        inline VarState &getVarState(const TraceEntry::VarInfo &info) { return getVarState(info.idx); }
+        inline VarState &operator[](unsigned varIdx) { return getVarState(varIdx); }
+        inline VarState &operator[](const TraceEntry::VarInfo &info) { return getVarState(info); }
+
+        void onCtor(unsigned opIdx, const TraceEntry::VarInfo &info);
+        void onUsed(unsigned opIdx, const TraceEntry::VarInfo &info);
+        void onDtor(unsigned opIdx, const TraceEntry::VarInfo &info);
+
+        void onChanged(unsigned opIdx, const TraceEntry::VarInfo &info,
+                       std::function<void (std::string &curExpr)> operation);
+        void onChanged(unsigned opIdx, const TraceEntry::VarInfo &info,
+                       const TraceEntry::VarInfo &otherInfo,
+                       std::function<void (std::string &curExpr, const std::string_view &otherExpr)> operation);
+
+    protected:
+        std::vector<VarState> vars{};
+
+    };
+#pragma endregion structs
+
 public:
     inline DotTraceVisualizer(const std::fs::path &outputFile) :
         TraceVisualizer(getTmpFileName(outputFile)),
@@ -118,10 +218,37 @@ protected:
     std::fs::path outputFile;
     DotOutput dot;
     helpers::CounterMap<const void *, unsigned> ptrCells{};
+    struct {
+        unsigned copies = 0;
+        unsigned moves = 0;
+    } stats{};
+    unsigned nodesCnt = 0;
+    std::vector<bool> nodesPresent{};
+    Vars vars{};
+
+    struct style {
+        static constexpr std::string_view edge_timeline_color = "red";
+        static constexpr std::string_view edge_timeline_style = "solid";
+
+        static constexpr std::string_view edge_lifespan_color = "black";
+        static constexpr std::string_view edge_lifespan_style = "dashed";
+
+        static constexpr std::string_view edge_usage_color = "#6060C0";
+        static constexpr std::string_view edge_usage_style = "solid";
+
+        // TODO
+    };
+
+    static constexpr char NODE_FMT[] = "node_%u";
 
 
     void beginLog();
     void endLog();
+
+    void dumpInfo();
+    void dumpLegend();
+    void dumpOrdering();
+    void dumpPadding();
 
     void logVarInfo(const TraceEntry::VarInfo &info);
     void logEntry(const Trace &trace, unsigned idx);
@@ -129,5 +256,12 @@ protected:
     static std::fs::path getTmpFileName(const std::fs::path &outputFile);
 
     void compileOutput(bool debug = false) const;
+
+    static std::string_view getFuncColor(unsigned level);
+
+    // This feels crotchy...
+    auto writeNode(unsigned entryIdx, bool box = false) -> decltype(dot.writeNode(""));
+
+    std::string buildVarInfo(const TraceEntry::VarInfo &info);
 
 };
