090895d4b2d778b1ea1f7fd841485c406e245e7e
abel1502 <terragame.abel@gmail.com>
2022-03-18 14:29:36 +0300

    Templatonks add storage

--- Templatonks/array.h
+++ Templatonks/array.h
@@ -2,47 +2,140 @@
 
 #include <ACL/general.h>
 #include <ACL/type_traits.h>
+#include <concepts>
+#include <new>
 
 
 namespace mylib {
 
 
+template <typename St, typename T>
+concept Storage = true || ((requires (std::remove_cvref_t<St> storage,
+                            const std::remove_cvref_t<St> const_storage,
+                            T item, size_t idx) {
+    // General requirements
+    St{};
+    storage.item(idx) = std::move(item);
+    { const_storage.item(idx) } -> std::convertible_to<const T &>;
+    { const_storage.size() } -> std::unsigned_integral;
+    { St::is_dynamic } -> std::same_as<bool>;
+}) && (!St::is_dynamic ||
+      requires (std::remove_cvref_t<St> storage,
+                const std::remove_cvref_t<St> const_storage,
+                T item, size_t idx) {
+    // Dynamic requirements
+    St{(size_t)1};
+    St{(size_t)1, (const T &)item};
+    { storage.expand_one() } -> std::same_as<T *>;
+    storage.remove_one();
+    storage.clear();
+}) && (St::is_dynamic ||
+      requires (std::remove_cvref_t<St> storage,
+                const std::remove_cvref_t<St> const_storage,
+                T item, size_t idx) {
+    // Static requirements
+    // (Mostly just ensuring constexpr-ness)
+    std::bool_constant<(std::declval<St>().size(), true)>();
+    std::bool_constant<(std::declval<const St>().item(1), true)>();
+    std::bool_constant<(std::declval<const St>().item(1) = std::move(std::declval<T>()), true)>();
+}));
+
+
 template <typename T_, template <typename T> typename Storage_>
-requires (!std::is_reference_v<T_>)
+requires (!std::is_reference_v<T_> &&
+          Storage<Storage_<T_>, T_>)
 class Array {
 public:
     using value_type = T_;
     using reference = value_type &;
     using const_reference = const value_type &;
-    using size_type = unsigned;
-    using difference_type = int;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
 
     #pragma region Protected helpers
 protected:
     using storage_type = Storage_<value_type>;
-    
-    template <typename T>
-    using _if_dynamic = std::enable_if_t<storage_type::is_dynamic, T>;
+
+    // template <typename T>
+    // using _if_dynamic = std::enable_if_t<storage_type::is_dynamic, T>;
+    //
+    // static_assert(!storage_type::is_dynamic || std::is_same_v<_if_dynamic<void>, void>,
+    //               "Unexpected _if_dynamic behavior");
 public:
     #pragma endregion Protected helpers
 
-    Array() : storage{} {}
+    Array() : storage() {}
 
-    Array(size_type size) : storage{size} {}
-
-    Array(size_type size, const value_type &value) : 
-        storage{size, value} {}
-
-    reference operator[](size_type idx) {
-        return storage.item(idx);
+    Array(size_type size) : storage{size} {
+        static_assert(storage.is_dynamic);
     }
 
-    const_reference operator[](size_type idx) const {
+    Array(size_type size, const value_type &value) :
+        storage(size, value) {
+        static_assert(storage.is_dynamic);
+    }
+
+    Array(std::initializer_list<value_type> values) :
+        storage(std::move(values)) {}
+
+    constexpr reference operator[](difference_type idx) {
+        size_type sz = storage.size();
+
+        REQUIRE(-(difference_type)sz <= idx && idx < (difference_type)sz);
+
+        return storage.item((idx + sz) % sz);
+    }
+
+    constexpr inline const_reference operator[](size_type idx) const {
         return (*const_cast<Array *>(this))[idx];
     }
 
-    _if_dynamic<void> push_back(abel::universal_ref<value_type> auto &&val) const {
-        storage.data(storage.size(), true) = std::forward<decltype(val)>(val);
+    void push_back(abel::universal_ref<value_type> auto &&val) {
+        static_assert(storage.is_dynamic);
+
+        value_type *cell = storage.expand_one();
+        assert(cell);
+
+        new (cell) value_type(FWD(val));
+    }
+
+    template <typename ... As>
+    void emplace_back(As &&... args) {
+        static_assert(storage.is_dynamic);
+
+        value_type *cell = storage.expand_one();
+        assert(cell);
+
+        new (cell) value_type(std::forward<As>(args)...);
+    }
+
+    void pop_back() {
+        static_assert(storage.is_dynamic);
+
+        storage.remove_one();
+    }
+
+    void clear() {
+        if constexpr (storage.is_dynamic) {
+            storage.clear();
+            assert(storage.size() == 0);
+        } else {
+            static_assert(std::is_default_constructible_v<value_type>);
+
+            for (size_type i = 0; i < storage.size(); ++i) {
+                storage.item(i) = value_type{};
+            }
+        }
+    }
+
+    constexpr size_type size() const {
+        return storage.size();
+    }
+
+    inline bool empty() const {
+        static_assert(storage.is_dynamic);
+
+        return size() == 0;
     }
 
 protected:
---
+++ Templatonks/storage.h
@@ -0,0 +1,175 @@
+#pragma once
+
+#include <ACL/general.h>
+#include <ACL/type_traits.h>
+#include <concepts>
+#include <stdexcept>
+#include <algorithm>
+#include <initializer_list>
+
+
+namespace mylib {
+
+
+template <typename T>
+class DynamicLinearStorage {
+protected:
+    static constexpr size_t DEFAULT_CAPACITY = 4;
+    static_assert(std::is_move_constructible_v<T>);
+
+public:
+    static constexpr bool is_dynamic = true;
+
+
+    DynamicLinearStorage() = default;
+
+    DynamicLinearStorage(size_t new_size) {
+        static_assert(std::is_default_constructible_v<T>);
+
+        ensure_capacity(new_size);
+
+        for (size_t i = 0; i < new_size; ++i) {
+            new (&data_[i]) T();
+            ++size_;
+        }
+    }
+
+    DynamicLinearStorage(size_t new_size, const T &val) {
+        static_assert(std::is_copy_constructible_v<T>);
+
+        ensure_capacity(new_size);
+
+        for (size_t i = 0; i < new_size; ++i) {
+            new (&data_[i]) T(val);
+            ++size_;
+        }
+    }
+
+    DynamicLinearStorage(std::initializer_list<T> values) {
+        ensure_capacity(values.size());
+
+        for (auto &val : values) {
+            new (&data_[size_++]) T(std::move(val));
+        }
+    }
+
+    ~DynamicLinearStorage() noexcept {
+        clear();
+        assert(!data_);
+    }
+
+    T &item(size_t idx) {
+        if (idx >= size_) {
+            throw std::out_of_range("Index too large");
+        }
+
+        return data_[idx];
+    }
+
+    inline const T &item(size_t idx) const {
+        return const_cast<DynamicLinearStorage *>(this)->data_(idx);
+    }
+
+    T *expand_one() {
+        ensure_capacity(size_ + 1);
+
+        return &data_[size_++];
+    }
+
+    void remove_one() {
+        if (size_ == 0) {
+            throw std::overflow_error("Cannot remove from empty storage");
+        }
+
+        data_[--size_].~T();
+    }
+
+    void clear() noexcept {
+        for (unsigned i = 0; i < size_; ++i) {
+            data_[i].~T();
+        }
+
+        size_ = 0;
+        capacity_ = 0;
+        free(data_);
+        data_ = nullptr;
+    }
+
+    inline size_t size() const {
+        return size_;
+    }
+
+protected:
+    T *data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+
+    void ensure_capacity(size_t desired) {
+        if (desired == 0) {
+            return;
+        }
+
+        if (!data_) {
+            assert(size_ == 0);
+            assert(capacity_ == 0);
+
+            capacity_ = std::max(desired, DEFAULT_CAPACITY);
+            data_ = (T *)calloc(capacity_, sizeof(T));
+
+            if (!data_) {
+                capacity_ = 0;
+                throw std::overflow_error("Out of memory");
+            }
+
+            return;
+        }
+
+        if (desired < capacity_) {
+            return;
+        }
+
+        size_t new_capacity = capacity_ * 2;
+        while (new_capacity < desired) {
+            new_capacity *= 2;
+        }
+
+        T *new_data = (T *)calloc(new_capacity, sizeof(T));
+        if (!new_data) {
+            throw std::overflow_error("Out of memory");
+        }
+
+        assert(new_capacity >= size_);
+
+        for (size_t i = 0; i < size_; ++i) {
+            try {
+                new (&new_data[i]) T(std::move(data_[i]));
+            } catch (...) {
+                // Special cleanup in case of an error during
+                // broken-invariant state
+
+                for (size_t j = 0; j < i; ++j) {
+                    new_data[j].~T();
+                }
+
+                for (size_t j = i; j < size_; ++j) {
+                    data_[j].~T();
+                }
+
+                size_ = 0;
+
+                throw;
+            }
+
+            data_[i].~T();
+        }
+
+        free(data_);
+        data_ = new_data;
+        capacity_ = new_capacity;
+    }
+
+};
+
+
+}
