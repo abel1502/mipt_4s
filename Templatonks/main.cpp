4615e3e25af9a938d30d8488fc543c0b2784c44a
abel1502 <terragame.abel@gmail.com>
2022-03-18 14:30:12 +0300

    Templatonks update testcase

--- Templatonks/main.cpp
+++ Templatonks/main.cpp
@@ -1,11 +1,20 @@
 #include <ACL/general.h>
 #include "array.h"
+#include "storage.h"
 
 
 int main() {
     abel::verbosity = 2;
 
     DBG("It works!");
+
+    mylib::Array<int, mylib::DynamicLinearStorage> arr{};
+
+    arr.push_back(123);
+
+    DBG("arr[-1] == %d", arr[-1]);
+
+    arr.pop_back();
 
     return 0;
 }
